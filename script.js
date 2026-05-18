f83edea30710ceb16e01c814e3b4cd0210a61061
kaligarystudio <kaligarystudio@gmail.com>
2026-05-17 22:42:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -29,8 +29,7 @@ form.addEventListener("submit", async function(e){
   // PEGA AQUÍ TU URL DE APPS SCRIPT
   // =============================
 
-  const scriptURL = "AKfycbxw3qQ76JnPjdW7RfNGtXDCx8gxKq-XjbpU6k58tqQ4wygVLBlqiTv0P45snNKJnpsS";
-
+const scriptURL = "https://script.google.com/macros/s/AKfycbxw3qQ76JnPjdW7RfNGtXDCx8gxKq-XjbpU6k58tqQ4wygVLBlqiTv0P45snNKJnpsS/exec";
   try {
 
     await fetch(scriptURL, {
